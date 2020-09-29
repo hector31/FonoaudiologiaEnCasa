f9d97a8892eb1b8f859c18dba3424b7297b5aeae
Hector Narvaez <h31313131@gmail.com>
2020-09-29 17:00:33 -0500

    todos los cambios se han subido a la pagina fondo completo, se comienza la parte de galeria y cambiar el editor de text para administrador

--- resources/views/components/home/header-nav/home-header.blade.php
+++ resources/views/components/home/header-nav/home-header.blade.php
@@ -1,6 +1,6 @@
 <div class="container d-flex align-items-center">
 
-    <h1 class=" mr-auto"><a href="{{ route('home') }}"><img class="img-fluid logop" src="img/fonoencasa_logo.png" alt=""></a></h1>
+    <h1 class=" mr-auto titulologo"><a href="{{ route('home') }}"><img class="img-fluid logop" src="img/fonoencasa_logo.png" alt=""></a></h1>
     <!-- Uncomment below if you prefer to use an image logo -->
     <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
